cfbfc7d5d8990e304d644af764b2a063ff6404cb
susahesumudu <sumudu.susahe@gmail.com>
2024-12-12 21:28:07 +0530

    Change2

--- enrollments/models.py
+++ enrollments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Batch:
+    pass
+
+class Payment:
+    pass
+
+class Certificate:
+    pass
--- schedules/models.py
+++ schedules/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class CoursePlan:
+    pass
+
+class TrainingPlan:
+    pass
+
+class LessonPlan:
+    pass
+
